e9a1b5cebc574768ec2ae4856c204e83a3cf7804
elvis <ternencelopez@gmail.com>
2024-04-19 19:27:34 +0800

    feature: 实现Base62短链接生成算法

---
+++ interviewQuestion/src/main/java/convertDecToBase62/convert.java
@@ -0,0 +1,30 @@
+package convertDecToBase62;
+
+import cn.hutool.core.lang.hash.MurmurHash;
+
+public class convert {
+    private static final char[] CHARS = new char[]{
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
+    private static final int SIZE = CHARS.length;
+
+    private static String convertDecToBase62(long num) {
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            int i = (int) (num % SIZE);
+            sb.append(CHARS[i]);
+            num /= SIZE;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        int hash32 = MurmurHash.hash32("https://nageoffer.com/shortlink");
+        System.out.println(hash32);
+        long actualHash32 = hash32 < 0 ? Integer.MAX_VALUE - (long) hash32 : hash32;
+        String shortLink = convertDecToBase62(actualHash32);
+        System.out.println(shortLink);
+    }
+}
